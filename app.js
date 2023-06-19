2644af5d8686a41de179b233f889fab6b055e4de
saito.kohei <saito.kohei@raccoon.ne.jp>
2023-06-19 19:15:34 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io').listen(http);
+const { WebClient } = require('@slack/web-api');
+const web = new WebClient(process.env.SLACK_TOKEN);
+const PORT = process.env.PORT || 80;
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+io.sockets.on('connection', function (socket) {
+});
+
+app.post('/slack', async function (req, res) {
+
+  const { type, event } = req.body;
+  if (type === 'challenge') {
+  } else if (type === 'event_callback') {
+    console.log(event);
+    const result = await web.conversations.info({
+      channel: event.channel
+    });
+
+    console.log(result.channel.name)
+
+    io.sockets.emit(result.channel.name, event.text);
+  }
+
+  res.status(200).json(req.body);
+
+});
+
+app.post('/comment', async function (req, res) {
+  io.sockets.emit('form', req.body.comment);
+  res.status(204).json();
+});
+
+app.get('/test', async function (req, res) {
+  console.log("test");
+  io.sockets.emit('message', "てすと");
+
+  res.send({ test: "てすと" });
+
+});
+
+http.listen(PORT, function () {
+  console.log('server listening. Port:' + PORT);
+});
---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+const successDiv = document.getElementById("success");
+const errorDiv = document.getElementById("error");
+const postButton = document.getElementById('postButton')
+const loading = document.getElementById('loadingWrap')
+
+postButton.addEventListener('click', () => {
+  const comment = document.getElementById('comment');
+  if (comment.value === "") return;
+
+  postButton.disabled = true
+  startLoading();
+
+  fetch('/comment', {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      comment: comment.value
+    })
+  }).then(res => {
+    if (res.ok) {
+      comment.value = ''
+      fadeInOutAnime(successDiv)
+    }
+  }).catch(error => {
+    console.log(error)
+    fadeInOutAnime(errorDiv)
+  }).finally(() => {
+    postButton.disabled = false
+    stopLoading();
+  })
+})
+
+const startLoading = () => {
+  loading.style.display = 'block';
+}
+
+const stopLoading = () => {
+  loading.style.display = 'none';
+}
+
+const fadeInOutAnime = elm => {
+  elm.classList.add("fadeIn");
+  setTimeout(() => {
+    elm.classList.remove("fadeIn");
+  }, 2000)
+}
